54f234b32a0a5d4a079a93016bca71b011a6c270
Damien <dbournac@yahoo.fr>
2019-01-29 12:05:45 +0100

    nouvel article + liste

---
+++ src/Form/ArticleType.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ArticleType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+
+            ->add('title', TextType::class, ['label' => 'Titre'])
+            ->add('subtitle', TextType::class, ['label' => 'Sous-titre'])
+            ->add('body', TextareaType::class, ['label' => 'Contenu de l\'article'])
+            ->add('image', FileType::class, [
+                'label' => 'Image de l\'article',
+                'mapped' => false,
+                'attr' => [
+                    'accept' => 'image/*'
+                ]])
+            ->add('videolink', TextType::class, ['label' => 'Lien vers la vidéo'])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'name',
+                'label' => 'Choisissez une catégorie'])
+            ->add('save', SubmitType::class, ['label' => 'Créer l\'article'])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Article::class,
+        ]);
+    }
+}
